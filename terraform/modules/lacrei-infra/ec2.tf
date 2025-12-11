d552786924da746f24adeee2ffca98c9f4a7bacc
Mage <magedbgt@gmail.com>
2025-12-11 14:29:03 -0300

    fix

--- terraform/modules/lacrei-infra/ec2.tf
+++ terraform/modules/lacrei-infra/ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "app" {
   associate_public_ip_address = true
 
   root_block_device {
-    volume_size           = 20
+    volume_size           = 30
     volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
