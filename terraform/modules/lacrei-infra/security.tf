5808b63040fdaea99cc9677801128c51b028b2de
Mage <magedbgt@gmail.com>
2025-12-12 03:37:50 -0300

    help

--- terraform/modules/lacrei-infra/security.tf
+++ terraform/modules/lacrei-infra/security.tf
@@ -132,3 +132,15 @@ resource "aws_security_group" "vpc_endpoints" {
     Name = "${var.project_name}-${var.environment}-vpc-endpoints-sg"
   }
 }
+
+# S3 Gateway Endpoint (allows S3 access without internet gateway)
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.public.id, aws_route_table.private.id]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-s3-endpoint"
+  }
+}
